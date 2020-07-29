d229ca0421d5addb082926972fa049bea71f0635
Aryan Sharma <avisharmaindia07@gmail.com>
2020-07-29 21:57:15 +0530

    Day-29 (Buy and sell stocks with cooldown)

---
+++ Day-29.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        free = 0
+        have = cool = float('-inf')
+        for p in prices:
+            free, have, cool = max(free, cool), max(have, free - p), have + p
+        return max(free, cool)
